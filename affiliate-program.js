bd7242c9ef9cfac329ac537753eba3d84ebf02a5
admin-dimconex <100717536+admin-dimconex@users.noreply.github.com>
2024-10-10 21:34:53 +0200

    Update affiliate-program.js

--- affiliate-program.js
+++ affiliate-program.js
@@ -1,20 +1,14 @@
-// Espera a que la página cargue completamente
 window.addEventListener('load', function() {
-    // Verifica si la URL contiene 'login' o 'register'
     const url = window.location.href;
     if (url.includes('login') || url.includes('register')) {
-        // Selecciona el elemento con el ID 'app'
         const appElement = document.querySelector('#app');
         
-        // Verifica que el elemento exista antes de intentar agregar contenido
         if (appElement) {
-            // Crea un nuevo div
             const newDiv = document.createElement('div');
+            newDiv.id = 'topbar';
             
-            // Asigna el contenido que desees al nuevo div
-            newDiv.innerHTML = 'Contenido para login o register';
+            newDiv.innerHTML = '<img src="https://dimconex-media.github.io/static-files/logo_affiliate_program.png"/><a href="//cathopic.com">Back to cathopic.com</a>';
             
-            // Inserta el nuevo div como hijo del elemento seleccionado
             appElement.appendChild(newDiv);
         } else {
             console.error('No se encontró el elemento con ID #app.');
