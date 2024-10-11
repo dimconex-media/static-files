0682c7a3ac9a23393bfdc31ae35a22074e2b2e3f
admin-dimconex <100717536+admin-dimconex@users.noreply.github.com>
2024-10-11 09:48:47 +0200

    Update affiliate-program.js

--- affiliate-program.js
+++ affiliate-program.js
@@ -9,7 +9,7 @@ window.addEventListener('load', function() {
             const newDiv = document.createElement('div');
             newDiv.id = 'topbar';
             
-            newDiv.innerHTML = '<img src="https://dimconex-media.github.io/static-files/logo_affiliate_program.png"/><div style="display:flex; align-items:center; gap: 24px;"><a href="//cathopic.com/affiliate-program" target="_blank">Program benefits</a><a href="//cathopic.com" target="_blank">cathopic.com ↗</a></div>';
+            newDiv.innerHTML = '<img src="https://dimconex-media.github.io/static-files/logo_affiliate_program.png"/><div style="display:flex; align-items:center; gap: 24px;"><a href="//cathopic.com/affiliate-program" target="_blank">Program benefits ↗</a></div>';
             
             appElement.appendChild(newDiv);
         } else {
